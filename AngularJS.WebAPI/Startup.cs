1444add83e60015efab620f3338845e2e2b1fc18
Santosh <santosh.ksingh03@gmail.com>
2015-01-01 18:09:17 +0530

    Initial commit

---
+++ AngularJS.WebAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularJS.WebAPI.Startup))]
+namespace AngularJS.WebAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
